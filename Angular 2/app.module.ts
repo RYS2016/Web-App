4654f531fd15d4556d647d44f1a7e8beb278f14a
Yaroslav <rykal2014@gmail.com>
2018-08-02 21:04:37 -0400

    Add files via upload

---
+++ Angular 2/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { Part1Component } from './part1/part1.component';
+import { Part2Component } from './part2/part2.component';
+import { TokenizerPipe } from './part2/tokenizer.pipe';
+
+import { LocalStorageModule } from 'angular-2-local-storage';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Part1Component,
+    Part2Component,
+    TokenizerPipe
+  ],
+  imports: [
+    BrowserModule, FormsModule, LocalStorageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
